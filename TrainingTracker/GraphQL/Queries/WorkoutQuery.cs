a99f002d6deb270b3b5cf255b44e898fa0029267
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-08-04 20:43:52 -0500

    Add workout graphQL query

---
+++ TrainingTracker/GraphQL/Queries/WorkoutQuery.cs
@@ -0,0 +1,13 @@
+﻿using TrainingTracker.Application.DTOs.GraphQL.Workout;
+using TrainingTracker.Application.Interfaces.Services;
+
+namespace TrainingTracker.API.GraphQL.Queries
+{
+    public class WorkoutQuery
+    {
+        public async Task<IEnumerable<WorkoutGraphQLDto>> GetWorkoutsByUser(int idUser, [Service] IWorkoutsService workoutsService)
+        {
+            return await workoutsService.GetWorkoutsByUser(idUser);
+        }
+    }
+}
