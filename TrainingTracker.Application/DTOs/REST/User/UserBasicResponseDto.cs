0446509f2c30b68ed4e399e05e00008827093782
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-08-22 00:15:49 -0500

    Add dto

---
+++ TrainingTracker.Application/DTOs/REST/User/UserBasicResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using TrainingTracker.Domain.Entities.ENUM;
+
+namespace TrainingTracker.Application.DTOs.REST.User
+{
+    public class UserBasicResponseDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public Gender Gender { get; set; }
+    }
+}
